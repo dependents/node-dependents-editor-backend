e705ba3ef66c072811bae7eeef604151b106778b
Joel Kemp <mrjoelkemp@gmail.com>
2016-05-16 10:12:29 -0400

    Tests: SCSS

--- test/lib/cli.js
+++ test/lib/cli.js
@@ -90,5 +90,68 @@ describe('lib/cli', function() {
         });
       });
     });
+
+    describe('scss', function() {
+      beforeEach(function() {
+        this._directory = `${this._fixturePath}/scss`;
+      });
+
+      it('resolves extensionless partials', function() {
+        return cli({
+          filename: `${this._directory}/site.scss`,
+          directory: this._directory,
+          args: ['vendors/_packages'],
+          lookup: true
+        }).then(result => {
+          assert.equal(result, `${this._directory}/vendors/_packages.scss`);
+        });
+      });
+
+      it('resolves non-underscored partials that map to an underscored filename', function() {
+        return cli({
+          filename: `${this._directory}/site.scss`,
+          directory: this._directory,
+          args: ['vendors/packages'],
+          lookup: true
+        }).then(result => {
+          assert.equal(result, `${this._directory}/vendors/_packages.scss`);
+        });
+      });
+
+      it('resolves partials with quotes and a trailing semicolon', function() {
+        return cli({
+          filename: `${this._directory}/site.scss`,
+          directory: this._directory,
+          args: ['"vendors/_packages.scss";'],
+          lookup: true
+        }).then(result => {
+          assert.equal(result, `${this._directory}/vendors/_packages.scss`);
+        });
+      });
+
+      it('resolves relative partials', function() {
+        return cli({
+          filename: `${this._directory}/vendors/_packages.scss`,
+          directory: this._directory,
+          args: ['../mixins'],
+          lookup: true
+        }).then(result => {
+          assert.equal(result, `${this._directory}/_mixins.scss`);
+        });
+      });
+
+      describe('when a partial\'s name matches a file in its directory and parent directory', function() {
+        it('resolves to the file in its directory', function() {
+          return cli({
+            filename: `${this._directory}/utils/_debug.scss`,
+            directory: this._directory,
+            args: ['mixins'],
+            lookup: true
+          }).then(result => {
+            assert.equal(result, `${this._directory}/utils/_mixins.scss`);
+          });
+        });
+      });
+    });
   });
 });
