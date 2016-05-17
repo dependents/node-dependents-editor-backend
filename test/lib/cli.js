7f6687380a72c82748aead45c35ff019a41bf41b
Joel Kemp <mrjoelkemp@gmail.com>
2016-05-16 21:31:49 -0400

    Tests: Lookup - Stylus

--- test/lib/cli.js
+++ test/lib/cli.js
@@ -153,5 +153,72 @@ describe('lib/cli', function() {
         });
       });
     });
+
+    describe('stylus', function() {
+      beforeEach(function() {
+        this._directory = `${this._fixturePath}/stylus`;
+      });
+
+      it('resolves .css partials', function() {
+        return cli({
+          filename: `${this._directory}/vendors/index.styl`,
+          directory: this._directory,
+          args: ['foo.css'],
+          lookup: true
+        }).then(result => {
+          assert.equal(result, `${this._directory}/vendors/foo.css`);
+        });
+      });
+
+      it('resolves partials in the same directory', function() {
+
+      });
+
+      it('resolves partials in subdirectories', function() {
+        return cli({
+          filename: `${this._directory}/site.styl`,
+          directory: this._directory,
+          args: ['vendors/foo.css'],
+          lookup: true
+        }).then(result => {
+          assert.equal(result, `${this._directory}/vendors/foo.css`);
+        });
+      });
+
+      it('resolves extensionless partials', function() {
+        return cli({
+          filename: `${this._directory}/site.styl`,
+          directory: this._directory,
+          args: ['utils/_functions'],
+          lookup: true
+        }).then(result => {
+          assert.equal(result, `${this._directory}/utils/_functions.styl`);
+        });
+      });
+
+      it('resolves partials with extensions', function() {
+        return cli({
+          filename: `${this._directory}/site.styl`,
+          directory: this._directory,
+          args: ['utils/_functions.styl'],
+          lookup: true
+        }).then(result => {
+          assert.equal(result, `${this._directory}/utils/_functions.styl`);
+        });
+      });
+
+      describe('when given a directory as a partial', function() {
+        it('resolves to the index.styl file of a directory', function() {
+          return cli({
+            filename: `${this._directory}/site.styl`,
+            directory: this._directory,
+            args: ['vendors'],
+            lookup: true
+          }).then(result => {
+            assert.equal(result, `${this._directory}/vendors/index.styl`);
+          });
+        });
+      });
+    });
   });
 });
