75d128de33ba74f7453c91c70232c67e6de496f8
Joel Kemp <mrjoelkemp@gmail.com>
2016-07-06 21:05:32 -0400

    Extract cli print to a testable method
    Fixes #26

--- test/lib/cli.js
+++ test/lib/cli.js
@@ -1,6 +1,7 @@
 import assert from 'assert';
 import path from 'path';
 import cli from '../../lib/cli';
+import sinon from 'sinon';
 
 describe('lib/cli', function() {
   beforeEach(function() {
@@ -22,6 +23,32 @@ describe('lib/cli', function() {
     }).catch(message => {
       assert.equal(message, 'No valid command given');
       done();
+    });
+  });
+
+  describe('.print', function() {
+    beforeEach(function() {
+      this._console = sinon.stub(console, 'log');
+    });
+
+    afterEach(function() {
+      this._console.restore();
+    });
+
+    it('logs the output to the console', function() {
+      cli.print('some result');
+      assert.ok(this._console.calledWith('some result'));
+    });
+
+    it('works with an array result', function() {
+      cli.print(['some result']);
+      assert.ok(this._console.calledWith('some result'));
+    });
+
+    it('logs objects as strings', function() {
+      const result = {foo: 'bar'};
+      cli.print(result);
+      assert.ok(this._console.calledWith(JSON.stringify(result)));
     });
   });
 });
